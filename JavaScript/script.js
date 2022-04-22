bf4a1970b3ea145ac30aebec3a987b8c19979af8
Michael K.A <michaelkwameappiahamankwah@gmail.com>
2022-04-22 02:32:01 +0100

    Delete JavaScript directory

--- JavaScript/script.js
+++
@@ -1,5 +0,0 @@
-const selectElement = (Element) => document.querySelector(Element);
-selectElement("#mobile-menu").addEventListener("click", () => {
-
-    selectElement("header").classList.toggle("active");
-});
