3a10e67f52235ec498314d8157d065d7685ed949
Michael K.A <michaelkwameappiahamankwah@gmail.com>
2022-04-22 02:21:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const selectElement = (Element) => document.querySelector(Element);
+selectElement("#mobile-menu").addEventListener("click", () => {
+
+    selectElement("header").classList.toggle("active");
+});
